bc5464bc7cdd289af56695bea25714ae7bef7b30
Sushanth Jayanth <sushanth.jayanth@gmail.com>
2023-03-09 15:14:10 -0500

    Create run_sim_docker.sh

---
+++ run_sim_docker.sh
@@ -0,0 +1,52 @@
+# Map host's display socket to docker
+DOCKER_ARGS+=("-v /tmp/.X11-unix:/tmp/.X11-unix")
+DOCKER_ARGS+=("-v $HOME/.Xauthority:/home/admin/.Xauthority:rw")
+DOCKER_ARGS+=("-e DISPLAY")
+DOCKER_ARGS+=("-e NVIDIA_VISIBLE_DEVICES=all")
+DOCKER_ARGS+=("-e NVIDIA_DRIVER_CAPABILITIES=all")
+DOCKER_ARGS+=("-e FASTRTPS_DEFAULT_PROFILES_FILE=/usr/local/share/middleware_profiles/rtps_udp_profile.xml")
+
+if [[ $PLATFORM == "aarch64" ]]; then
+    DOCKER_ARGS+=("-v /usr/bin/tegrastats:/usr/bin/tegrastats")
+    DOCKER_ARGS+=("-v /tmp/argus_socket:/tmp/argus_socket")
+    DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcusolver.so.11:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libcusolver.so.11")
+    DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcusparse.so.11:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libcusparse.so.11")
+    DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcurand.so.10:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libcurand.so.10")
+    DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libnvToolsExt.so:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libnvToolsExt.so")
+    DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcupti.so.11.4:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libcupti.so.11.4")
+    DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/include:/usr/local/cuda-11.4/targets/aarch64-linux/include")
+    DOCKER_ARGS+=("-v /usr/lib/aarch64-linux-gnu/tegra:/usr/lib/aarch64-linux-gnu/tegra")
+    DOCKER_ARGS+=("-v /usr/src/jetson_multimedia_api:/usr/src/jetson_multimedia_api")
+    DOCKER_ARGS+=("-v /opt/nvidia/nsight-systems-cli:/opt/nvidia/nsight-systems-cli")
+    DOCKER_ARGS+=("--pid=host")
+    DOCKER_ARGS+=("-v /opt/nvidia/vpi2:/opt/nvidia/vpi2")
+    DOCKER_ARGS+=("-v /usr/share/vpi2:/usr/share/vpi2")
+    DOCKER_ARGS+=("-v /run/jtop.sock:/run/jtop.sock:ro")
+fi
+
+# Optionally load custom docker arguments from file
+DOCKER_ARGS_FILE="$ROOT/.isaac_ros_dev-dockerargs"
+if [[ -f "$DOCKER_ARGS_FILE" ]]; then
+    print_info "Using additional Docker run arguments from $DOCKER_ARGS_FILE"
+    readarray -t DOCKER_ARGS_FILE_LINES < $DOCKER_ARGS_FILE
+    for arg in "${DOCKER_ARGS_FILE_LINES[@]}"; do
+        DOCKER_ARGS+=($(eval "echo $arg | envsubst"))
+    done
+fi
+
+# Run container from image
+print_info "Running $CONTAINER_NAME"
+docker run -it --rm \
+    --privileged \
+    --network host \
+    ${DOCKER_ARGS[@]} \
+    -v $ISAAC_ROS_DEV_DIR:/workspaces/isaac_ros-dev \
+    -v /dev/*:/dev/* \
+    --name "$CONTAINER_NAME" \
+    --runtime nvidia \
+    --user="admin" \
+    --entrypoint /usr/local/bin/scripts/workspace-entrypoint.sh \
+    --workdir /workspaces/isaac_ros-dev \
+    $@ \
+    $BASE_NAME \
+    /bin/bash
